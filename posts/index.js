4cc674a407d0e71023e5ee064e09c448fb908212
souviks72 <souvikmandalvit2016@gmail.com>
2023-04-07 00:38:32 +0530

    using k8s to build services

--- posts/index.js
+++ posts/index.js
@@ -24,7 +24,7 @@ app.post("/posts", async (req, res) => {
     title,
   };
 
-  await axios.post("http://localhost:4005/events", {
+  await axios.post("http://event-bus-serv:4005/events", {
     type: "PostCreated",
     data: {
       id,
@@ -42,5 +42,6 @@ app.post("/events", (req, res) => {
 });
 
 app.listen(4000, () => {
+  console.log("v2");
   console.log("Listening on 4000");
 });
