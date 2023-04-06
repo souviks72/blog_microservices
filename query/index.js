646f810d8b6772035773e7dd1b3a8291b65c6315
souviks72 <souvikmandalvit2016@gmail.com>
2023-04-06 23:12:06 +0530

    building a mini microservices app

---
+++ query/index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+import { axios } from "axios";
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const posts = {};
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const handleEvent = (type, data) => {
+  if (type === "PostCreated") {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { id, content, postId, status } = data;
+
+    posts[postId].comments.push({ id, content, status });
+  }
+
+  if (type === "CommentUpdated") {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => comment.id === id);
+    comment.status = status;
+    comment.content = content;
+  }
+};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  handleEvent(type, data);
+
+  console.log(posts);
+  res.send({});
+});
+
+app.listen(4002, async () => {
+  console.log("listening on port 4002");
+
+  const res = await axios
+    .get("http://localhost4005/events")
+    .catch((err) => console.log(err));
+
+  for (let event of res.data) {
+    console.log("Processing event: ".event.type);
+    handleEvent(event.type, event.data);
+  }
+});
